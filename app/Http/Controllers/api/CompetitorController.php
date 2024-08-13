d40f62f4e72967b4f0a7f4550f29129864d70547
Wassim-Hallack <alhallakwassem098@gmail.com>
2024-08-13 05:08:47 +0300

    report

--- app/Http/Controllers/api/CompetitorController.php
+++ app/Http/Controllers/api/CompetitorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Competition;
+use App\Models\MobileUser;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
